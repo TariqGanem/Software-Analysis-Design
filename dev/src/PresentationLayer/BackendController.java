4dae759458da9a179ff963c444431c3e320301f9
Harel Afriat <harelaf@post.bgu.ac.il>
2021-04-01 14:52:10 +0300

    Implemeneted IOController and made a constructor for BackendController

--- dev/src/PresentationLayer/BackendController.java
+++ dev/src/PresentationLayer/BackendController.java
@@ -10,5 +10,9 @@ import BusinessLayer.ShiftPackage.Shift;
 public class BackendController {
     private Facade facade;
     private IOController ioController;
-    
+
+    public BackendController() {
+        facade = new Facade();
+        ioController = IOController.getInstance();
+    }
 }
--- dev/src/PresentationLayer/IOController.java
+++ dev/src/PresentationLayer/IOController.java
@@ -1,9 +1,35 @@
 package PresentationLayer;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class IOController {
+    private static IOController controller = null;
     private Scanner scanner;
 
-    //TODO: IMPLEMENT AS SINGLETON
+    private IOController() {
+        scanner = new Scanner(System.in);
+    }
+
+    public static IOController getInstance() {
+        if (controller == null)
+            controller = new IOController();
+        return controller;
+    }
+
+    public String getString() {
+        return scanner.next();
+    }
+
+    public int getInt() {
+        return scanner.nextInt();
+    }
+
+    public void print(String str) {
+        System.out.print(str);
+    }
+
+    public void println(String str) {
+        System.out.println(str);
+    }
 }
