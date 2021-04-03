c4f2e2add4012d82dfdf333d567a7da037f6649a
Luee Akasha <luee@post.bgu.ac.il>
2021-04-04 00:47:12 +0300

    presentation objects were added!

---
+++ dev/src/Presentation_Layer/Objects/PresentationObject.java
@@ -0,0 +1,5 @@
+package Presentation_Layer.Objects;
+
+public interface PresentationObject {
+	public void view();
+}
---
+++ dev/src/Presentation_Layer/Objects/Supplier.java
@@ -0,0 +1,11 @@
+package Presentation_Layer.Objects;
+
+public class Supplier implements PresentationObject {
+
+	@Override
+	public void view() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
