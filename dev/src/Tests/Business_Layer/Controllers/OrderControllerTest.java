20cbef141f6414fc9bc57edd9c77de9f99f31fc4
Luee Akasha <luee@post.bgu.ac.il>
2021-04-08 15:58:33 +0300

    unit testing for Business controllers was defined!

---
+++ dev/src/Tests/Business_Layer/Controllers/OrderControllerTest.java
@@ -0,0 +1,38 @@
+package Business_Layer.Controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void placeAnOrder() {
+    }
+
+    @Test
+    void completeAnOrder() {
+    }
+
+    @Test
+    void cancelAnOrder() {
+    }
+
+    @Test
+    void rescheduleAnOrder() {
+    }
+}
---
+++ dev/src/Tests/Business_Layer/Controllers/SupplierControllerTest.java
@@ -0,0 +1,66 @@
+package Business_Layer.Controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void addSupplier() {
+    }
+
+    @Test
+    void removeSupplier() {
+    }
+
+    @Test
+    void changePaymentConditions() {
+    }
+
+    @Test
+    void changeBankAccount() {
+    }
+
+    @Test
+    void printSupplierCard() {
+    }
+
+    @Test
+    void addContactPerson() {
+    }
+
+    @Test
+    void removeContact() {
+    }
+
+    @Test
+    void printAllContacts() {
+    }
+
+    @Test
+    void addMethod() {
+    }
+
+    @Test
+    void removeMethod() {
+    }
+
+    @Test
+    void editMethod() {
+    }
+}
