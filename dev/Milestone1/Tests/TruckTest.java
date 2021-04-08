0aae796a4b70c3038cea8b352f3f5b376a8c01d3
TariqGanem <tariqg@post.bgu.ac.il>
2021-04-08 21:35:40 +0300

    Completing TruckTest

--- dev/Milestone1/Tests/TruckTest.java
+++ dev/Milestone1/Tests/TruckTest.java
@@ -1,5 +1,4 @@
 import BusinessLayer.Facade;
-import BusinessLayer.Objects.Truck;
 import BusinessLayer.Response;
 import org.junit.After;
 import org.junit.Before;
@@ -19,20 +18,36 @@ public class TruckTest {
     public void testAddTruck(){
         data.addTruck("12345", "BMW", 6.5, 12);
         assertEquals(1, data.getAlltrucks().getValue().size());
-        tearDown();
-    }
-
-    @Test
-    public void testTruckWeight(){
-        Response res =  data.addTruck("12345", "BMW", 6.5, 6.2);
-        assertEquals(res.getMsg(), "Couldn't add new truck - Illegal truck weight");
-        assertEquals(0, data.getAlltrucks().getValue().size());
-        tearDown();
+        assertEquals(data.getAlltrucks().getValue().get(0).getTruckPlateNumber(), "12345");
+        assertEquals(data.getAlltrucks().getValue().get(0).getModel(), "BMW");
+        assertEquals(6.5, data.getAlltrucks().getValue().get(0).getNatoWeight(), 0.0);
+        assertEquals(12, data.getAlltrucks().getValue().get(0).getMaxWeight(), 0.0);
     }
 
     @Test
     public void testAddTruckWithSameId(){
+        data.addTruck("12345", "BMW", 6.5, 12);
+        Response res = data.addTruck("12345", "Volvo", 4, 8);
+        assertEquals(res.getMsg(), "Couldn't add new truck - truckPlateNumber already exists");
+        assertNotEquals(2, data.getAlltrucks().getValue().size());
+    }
 
+    @Test
+    public void testTruckWeight(){
+        Response res1 =  data.addTruck("12345", "BMW", 6.5, 6.2);
+        Response res2 =  data.addTruck("123456", "BMWW", -1, -3);
+        assertEquals("Couldn't add new truck - Illegal truck weight", res1.getMsg());
+        assertEquals("Couldn't add new truck - Illegal truck weight", res2.getMsg());
+        assertEquals(0, data.getAlltrucks().getValue().size());
+    }
+
+    @Test
+    public void testEmptyOrNullFields(){
+        Response res1 =  data.addTruck("", "BMW", 6, 10);
+        Response res2 =  data.addTruck("123456", null, 5, 12);
+        assertEquals("Couldn't add new truck - Invalid parameters", res1.getMsg());
+        assertEquals("Couldn't add new truck - Invalid parameters", res2.getMsg());
+        assertEquals(0, data.getAlltrucks().getValue().size());
     }
 
     @After
