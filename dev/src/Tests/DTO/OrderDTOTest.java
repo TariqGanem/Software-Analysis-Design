2bb51149a6b938364395b3259641f39689da288a
Luee Akasha <luee@post.bgu.ac.il>
2021-04-08 15:55:54 +0300

    unit testing for DTO's was defined!

---
+++ dev/src/Tests/DTO/OrderDTOTest.java
@@ -0,0 +1,38 @@
+package DTO;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderDTOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getStatus() {
+    }
+
+    @Test
+    void getPlacementDate() {
+    }
+
+    @Test
+    void getOrderDate() {
+    }
+
+    @Test
+    void getItems() {
+    }
+}
