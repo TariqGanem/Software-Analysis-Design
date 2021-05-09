ae752e1f53e5ff32582fb6516b0e4fb7938978b5
Luee Akasha <luee@post.bgu.ac.il>
2021-05-10 02:52:07 +0300

    persistence function were impelemented!

--- dev/adss2020_v02/src/main/java/DataAccessLayer/Mappers/OrdersMapper.java
+++ dev/adss2020_v02/src/main/java/DataAccessLayer/Mappers/OrdersMapper.java
@@ -1,8 +1,13 @@
 package DataAccessLayer.Mappers;
 
+import DTO.ItemDTO;
+import DTO.OrderDTO;
+import DataAccessLayer.Objects.Item;
 import DataAccessLayer.Objects.Order;
+import Enums.Status;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,12 +19,23 @@ public class OrdersMapper extends Mapper{
         orders = new HashMap<>();
         try {
             Statement statement = connection.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS FixedOrders"+
-                    ""+""+
-                    ""+""+
-                    ""+""+
-                    ""+"";
+            String sql = "CREATE TABLE IF NOT EXISTS Orders " +
+                    "(Id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "Status                 TEXT    NOT NULL, " +
+                    "PlacementDate           TEXT    NOT NULL, " +
+                    "DueDate          TEXT     NOT NULL, "+
+                    "Fixed  INT NOT NULL)";
+            statement.executeUpdate(sql);
 
+            statement = connection.createStatement();
+            sql = "CREATE TABLE IF NOT EXISTS ItemsInOrders "+
+                    "(OrderID INT, "+
+                    "ItemID INT, " +
+                    "Amount INT NOT NULL"+
+                    "PRIMARY KEY(OrderID,ItemID)," +
+                    "FOREIGN KEY(OrderID) REFERENCES Orders(Id))";
+            statement.executeUpdate(sql);
+            
 
             statement.close();
             connection.close();
@@ -27,37 +43,74 @@ public class OrdersMapper extends Mapper{
         catch (SQLException exception) {
             exception.printStackTrace();
         }
-
     }
 
-    public int storeFixedOrder(){
+    public int storeFixedOrder(OrderDTO orderDTO){  // done
+        int output = -1;
         try {
-            Statement statement = connection.createStatement();
-            String sql = "";
+            String sql = "INSERT INTO Orders (Status,PlacementDate,DueDate,Fixed) \" +\n" +
+                    "                    \"VALUES (?,?,?,?)";
+            connection = connect();
 
-            statement.close();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,orderDTO.getStatus().toString());
+            preparedStatement.setString(2, orderDTO.getPlacementDate().toString());
+            preparedStatement.setString(3, orderDTO.getDueDate().toString());
+            preparedStatement.setInt(4, 1);
+            preparedStatement.executeUpdate();
+
+
+            output = getNewOrderID();
+            orderDTO.setId(output);
+            orders.putIfAbsent(orderDTO.getId(), new Order(orderDTO));
+
+            for (Map.Entry<Integer, ItemDTO> item: orderDTO.getItems().entrySet()) {
+                storeItemInOrder(output, item.getValue().getId(), item.getValue().getAmount());
+            }
+
+            preparedStatement.close();
             connection.close();
         }
-        catch (SQLException exception) {
+        catch (Exception exception) {
             exception.printStackTrace();
         }
-        return getNewOrderID();
-
+        return output;
     }
-    public int storeSingleOrder(){
+
+
+    public int storeSingleOrder(OrderDTO orderDTO){  // done
+        int output = -1;
         try {
-            Statement statement = connection.createStatement();
-            String sql = "";
+            String sql = "INSERT INTO Orders (Status,PlacementDate,DueDate,Fixed) \" +\n" +
+                    "                    \"VALUES (?,?,?,?)";
+            connection = connect();
 
-            statement.close();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,orderDTO.getStatus().toString());
+            preparedStatement.setString(2, orderDTO.getPlacementDate().toString());
+            preparedStatement.setString(3, orderDTO.getDueDate().toString());
+            preparedStatement.setInt(4, 0);
+            preparedStatement.executeUpdate();
+
+
+            output = getNewOrderID();
+            orderDTO.setId(output);
+            orders.putIfAbsent(orderDTO.getId(), new Order(orderDTO));
+
+            for (Map.Entry<Integer, ItemDTO> item: orderDTO.getItems().entrySet()) {
+                storeItemInOrder(output, item.getValue().getId(), item.getValue().getAmount());
+            }
+
+            preparedStatement.close();
             connection.close();
         }
-        catch (SQLException exception) {
+        catch (Exception exception) {
             exception.printStackTrace();
         }
-        return getNewOrderID();
-
+        return output;
     }
+
+
     public void LoadOrders(){
         try {
             Statement statement = connection.createStatement();
@@ -86,31 +139,98 @@ public class OrdersMapper extends Mapper{
         }*/
         return 1;
     }
-    public void updateFixedOrder(){
-        try {
-            Statement statement = connection.createStatement();
-            String sql = "";
+    public void updateStatus(int orderID, Status newStatus){   // done
+        try{
+            String sql = "UPDATE Orders " +
+                    "SET    Status = ? " +
+                    "WHERE  Id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, newStatus.toString());
+            preparedStatement.setInt(2, orderID);
+            preparedStatement.executeUpdate();
 
-
-            statement.close();
+            preparedStatement.close();
             connection.close();
         }
-        catch (SQLException exception) {
+        catch (SQLException exception){
             exception.printStackTrace();
         }
     }
-    public void updateSingleOrder(){
-        try {
-            Statement statement = connection.createStatement();
-            String sql = "";
+    public void updateDueDate(int orderID, LocalDate newDueDate){  // done
+        try{
+            String sql = "UPDATE Orders " +
+                    "SET    DueDate = ? " +
+                    "WHERE  Id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, newDueDate.toString());
+            preparedStatement.setInt(2, orderID);
+            preparedStatement.executeUpdate();
 
-            statement.close();
+            preparedStatement.close();
+            connection.close();
+        }
+        catch (SQLException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public void updateAmount(int orderID, int itemID, int newAmount){  // done
+        try{
+            String sql = "UPDATE ItemsInOrders " +
+                    "SET    Amount = ? " +
+                    "WHERE  OrderID = ? AND ItemID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, newAmount);
+            preparedStatement.setInt(2, orderID);
+            preparedStatement.setInt(3, itemID);
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            connection.close();
+        }
+        catch (SQLException exception){
+            exception.printStackTrace();
+        }
+    }
+
+
+    public void storeItemInOrder(int OrderID, int ItemID, int Amount){  // done
+        try{
+            String sql = "INSERT INTO ItemsInOrders (OrderID, ItemID, Amount) " +
+                         "VALUES(?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, OrderID);
+            preparedStatement.setInt(2, ItemID);
+            preparedStatement.setInt(3, Amount);
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            connection.close();
+        }
+        catch (Exception exception){
+            exception.printStackTrace();
+        }
+    }
+
+
+    public void removeItem(int orderID, int itemID){   // done
+        try{
+            String sql = "DELETE FROM ItemsInOrders " +
+                    "WHERE OrderID = ? AND ItemID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, orderID);
+            preparedStatement.setInt(2, itemID);
+
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
             connection.close();
 
         }
-        catch (SQLException exception) {
+        catch (SQLException exception){
             exception.printStackTrace();
         }
+
     }
 
 
